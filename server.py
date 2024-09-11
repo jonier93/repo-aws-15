eabf2c8fd08d565cc6ac5f922af64aed3ad54e8b
EC2 Default User <ec2-user@ip-172-31-43-155.us-east-2.compute.internal>
2024-09-11 00:30:44 +0000

    version1

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("register.html")
+     
+if __name__ == "__main__":    
+    host = "127.0.0.1"
+    port = 8000
+    app.run(host, port, True)
---
+++ static/js/appRegister.js
@@ -0,0 +1,4 @@
+
+function register_user() {
+    alert("Register button clicked")
+}
